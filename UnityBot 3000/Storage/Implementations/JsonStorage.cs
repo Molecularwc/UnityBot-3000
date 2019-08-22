940fce25c045107c0d86b69c8109f443828ada1b
Richard Salvaty <richardsalvaty@hotmail.com>
2019-08-22 15:15:03 -0700

    Create JsonStorage.cs

---
+++ UnityBot 3000/Storage/Implementations/JsonStorage.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace UnityBot_3000.Storage.Implementations
+{
+    public class JsonStorage : IDataStorage
+    {
+        public void StoreObject(object obj, string key)
+        {
+            var file = $"{key}.json";
+            Directory.CreateDirectory(file);
+        }
+
+        public T RestoreObject<T>(string key)
+        {
+            var json = File.ReadAllText($"{key}.json");
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+    }
+}
