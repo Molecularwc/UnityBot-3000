75d722e65c2ff51c627875ae37c24dc408f6a137
Richard Salvaty <richardsalvaty@hotmail.com>
2019-08-23 13:49:15 -0700

    Update UtilityTests.cs

--- UnityBot3K.xUnit.Tests/UtilityTests.cs
+++ UnityBot3K.xUnit.Tests/UtilityTests.cs
@@ -5,7 +5,7 @@ namespace UnityBot3K.xUnit.Tests
     public class UtilityTests
     {
         [Fact]
-        public void UnityBot3kFirstTest()
+        public void UnityBot3KFirstTest()
         {
         }
     }
